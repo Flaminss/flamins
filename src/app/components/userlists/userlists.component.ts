702f8629e4f1bb67e7db7f853347efd5b1eabc68
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-10-19 04:34:21 +0500

    header-done

---
+++ src/app/components/userlists/userlists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userlists',
+  standalone: true,
+  imports: [],
+  templateUrl: './userlists.component.html',
+  styleUrl: './userlists.component.css'
+})
+export class UserlistsComponent {
+
+}
