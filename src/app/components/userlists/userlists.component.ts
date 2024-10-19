3bb040958333cb6c52e8ae1c698a1cc7bf322fdf
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-10-19 20:07:40 +0500

    added-cards

--- src/app/components/userlists/userlists.component.ts
+++ src/app/components/userlists/userlists.component.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-userlists',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './userlists.component.html',
-  styleUrl: './userlists.component.css'
+  styleUrl: './userlists.component.css',
 })
 export class UserlistsComponent {
+  userlists: any[] = [
+    { Username: 'JohnDoe', wagered: 12000, Reward: 1500 },
+    { Username: 'JaneSmith', wagered: 13000, Reward: 1800 },
+    { Username: 'AliceJones', wagered: 14000, Reward: 2000 },
+    { Username: 'BobBrown', wagered: 15000, Reward: 2200 },
+    { Username: 'CharlieWhite', wagered: 16000, Reward: 2500 },
+    { Username: 'DianaBlack', wagered: 17000, Reward: 3000 },
+    { Username: 'EvanBlue', wagered: 18000, Reward: 3500 },
+    { Username: 'FionaGreen', wagered: 19000, Reward: 4000 },
+    { Username: 'GeorgeRed', wagered: 20000, Reward: 4500 },
+    { Username: 'HannahPurple', wagered: 21000, Reward: 5000 },
+  ];
 
-  constructor(private userService: UserService){
+  constructor(private userService: UserService) {}
 
+  reAdjustUserName(name: any) {
+    if (name.length > 4) {
+      return name.slice(0, 4) + '*'.repeat(name.length - 4);
+    }
+    return name; 
   }
-
   ngOnInit() {
     this.userService.getUserStats().subscribe(
-      data => console.log(data),
-      error => console.error(error)
+      (data) => console.log(data),
+      (error) => console.error(error)
     );
   }
- 
-
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,11 +7,12 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   // private apiUrl = 'https://roobetconnect.com/affiliate/stats'; 
+  // private apiUrl = '/api/affiliate/v2/stats';
   private apiUrl = '/api/affiliate/stats';
   constructor(private http: HttpClient) {}
 
   getUserStats() {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3NmExZTIxLTA5YTgtNGZhNS04ODY2LWFhMjBkYjQwYTZjNCIsIm5vbmNlIjoiMmVlMTkwYjEtMWI5MC00YTkyLWIxMWMtNDRhMTAzNTljNmIwIiwic2VydmljZSI6ImFmZmlsaWF0ZVN0YXRzIiwiaWF0IjoxNzI0MzQzNDc5fQ.Ye7nWH0CqYCwQjoC6JlTTv5N6ki3D1FdN0Jc8_-do1w';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3NmExZTIxLTA5YTgtNGZhNS04ODY2LWFhMjBkYjQwYTZjNCIsIm5vbmNlIjoiMmVlMTkwYjEtMWI5MC00YTkyLWIxMWMtNDRhMTAzNTljNmIwIiwic2VydmljZSI6ImFmZmlsaWF0ZVN0YXRzIiwiaWF0IjoxNzI0MzQzNDc5fQ.qjVt2sy2T30xVatftJZi8nBLC6gttcXc5ZMAIkRHuHk';
 
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${token}`)
