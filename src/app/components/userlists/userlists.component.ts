75729fceae01fa525b01adc77f72bd97ffaa8123
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-10-20 16:39:40 +0500

    small-fixes

--- src/app/components/userlists/userlists.component.ts
+++ src/app/components/userlists/userlists.component.ts
@@ -11,18 +11,6 @@ import { IntegerPipe } from '../../pipe/integer.pipe';
   styleUrl: './userlists.component.css',
 })
 export class UserlistsComponent {
-  // userlists: any[] = [
-  //   { Username: 'JohnDoe', wagered: 12000, Reward: 1500 },
-  //   { Username: 'JaneSmith', wagered: 13000, Reward: 1800 },
-  //   { Username: 'AliceJones', wagered: 14000, Reward: 2000 },
-  //   { Username: 'BobBrown', wagered: 15000, Reward: 2200 },
-  //   { Username: 'CharlieWhite', wagered: 16000, Reward: 2500 },
-  //   { Username: 'DianaBlack', wagered: 17000, Reward: 3000 },
-  //   { Username: 'EvanBlue', wagered: 18000, Reward: 3500 },
-  //   { Username: 'FionaGreen', wagered: 19000, Reward: 4000 },
-  //   { Username: 'GeorgeRed', wagered: 20000, Reward: 4500 },
-  //   { Username: 'HannahPurple', wagered: 21000, Reward: 5000 },
-  // ];
   userlists: any;
   finalList: any[] = [];
 
