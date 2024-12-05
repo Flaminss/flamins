ed7ae15b3a6cbcc27012db151e5f55075cb43240
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-12-06 02:12:30 +0500

    done

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,15 +14,33 @@ export class UserService {
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${token}`)
       .set('Content-Type', 'application/json');
-  
+
     const params = new HttpParams()
       .set('userId', '876a1e21-09a8-4fa5-8866-aa20db40a6c4')
       .set('startDate', startDate)
       .set('endDate', endDate)
-      .set('limit', '10'); // Only fetch top 10 users
-  
+      .set('limit', '1000');
+    // .set('timestamp', new Date().getTime().toString());
+
     return this.http.get(this.apiUrl, { headers, params });
   }
+
+  //working
+  // getUserStats(startDate: string, endDate: string) {
+  //   const token =
+  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3NmExZTIxLTA5YTgtNGZhNS04ODY2LWFhMjBkYjQwYTZjNCIsIm5vbmNlIjoiMmVlMTkwYjEtMWI5MC00YTkyLWIxMWMtNDRhMTAzNTljNmIwIiwic2VydmljZSI6ImFmZmlsaWF0ZVN0YXRzIiwiaWF0IjoxNzI0MzQzNDc5fQ.Ye7nWH0CqYCwQjoC6JlTTv5N6ki3D1FdN0Jc8_-do1w';
+  //   const headers = new HttpHeaders()
+  //     .set('Authorization', `Bearer ${token}`)
+  //     .set('Content-Type', 'application/json');
+  
+  //   const params = new HttpParams()
+  //     .set('userId', '876a1e21-09a8-4fa5-8866-aa20db40a6c4')
+  //     .set('startDate', startDate)
+  //     .set('endDate', endDate)
+  //     .set('limit', '10'); // Only fetch top 10 users
+  
+  //   return this.http.get(this.apiUrl, { headers, params });
+  // }
   // getUserStats() {
   //   const token =
   //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3NmExZTIxLTA5YTgtNGZhNS04ODY2LWFhMjBkYjQwYTZjNCIsIm5vbmNlIjoiMmVlMTkwYjEtMWI5MC00YTkyLWIxMWMtNDRhMTAzNTljNmIwIiwic2VydmljZSI6ImFmZmlsaWF0ZVN0YXRzIiwiaWF0IjoxNzI0MzQzNDc5fQ.Ye7nWH0CqYCwQjoC6JlTTv5N6ki3D1FdN0Jc8_-do1w';
